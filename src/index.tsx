f742571fd1cc3dca5e6914113b63430a0ffa7836
Diana Olarte <diana.olarte@edunext.co>
2025-09-24 12:22:04 +1000

    fix: add QueryClientProvider to the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import {
   APP_INIT_ERROR, APP_READY, subscribe, initialize,
@@ -11,16 +12,20 @@ import messages from './i18n';
 import './index.scss';
 import AuthZModule from 'authz-module';
 
+const queryClient = new QueryClient();
+
 subscribe(APP_READY, () => {
   const root = createRoot(document.getElementById('root') as HTMLElement);
 
   root.render(
     <StrictMode>
-      <AppProvider>
-        <Routes>
-          <Route path="/authz/*" element={<AuthZModule />} />
-        </Routes>
-      </ AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <Routes>
+            <Route path="/authz/*" element={<AuthZModule />} />
+          </Routes>
+        </ AppProvider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 });
