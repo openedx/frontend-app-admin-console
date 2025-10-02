4fb18b6b144191393ca594e20689ca9a9dc8676f
Diana Olarte <diana.olarte@edunext.co>
2025-10-02 23:09:41 +1000

    feat: add roles tab

--- src/authz-module/components/RoleCard/PermissionsRow.tsx
+++ src/authz-module/components/RoleCard/PermissionsRow.tsx
@@ -17,21 +17,25 @@ interface PermissionRowProps {
 
 const PermissionRow = ({ resourceLabel, actions }: PermissionRowProps) => (
   <Row className="row align-items-center border px-2 py-2">
-    <Col md={3}>
+    <Col md={2}>
       <span className="small font-weight-bold">{resourceLabel}</span>
     </Col>
     <Col>
-      <div className="w-100 d-flex flex-wrap">
-        {actions.map(action => (
-          <Chip
-            key={action.key}
-            iconBefore={actionsDictionary[action.key as ActionKey] as ComponentType}
-            disabled={action.disabled}
-            className="mr-4 my-2 px-3 bg-primary-100 border-0 permission-chip"
-            variant="light"
-          >
-            {action.label}
-          </Chip>
+      <div className="w-100 d-flex flex-wrap align-items-center">
+        {actions.map((action, index) => (
+          <>
+            <Chip
+              key={action.key}
+              iconBefore={actionsDictionary[action.key as ActionKey] as ComponentType}
+              disabled={action.disabled}
+              className="mx-3 my-2 px-3 bg-primary-100 border-0 permission-chip"
+              variant="light"
+            >
+              {action.label}
+            </Chip>
+            {(index === actions.length - 1) ? null
+              : (<hr className="border-right mx-2" style={{ height: '24px' }} />)}
+          </>
         ))}
       </div>
     </Col>
