c109062979aefafd2eed5b3fc7e95392e6886b59
Jesus Balderrama <jesus.balderrama.wgu@gmail.com>
2026-03-23 12:05:22 -0600

    feat: refactor to create new path for roles permissions tab

--- src/authz-module/libraries-manager/constants.ts
+++ src/authz-module/libraries-manager/constants.ts
@@ -5,16 +5,20 @@ export const CONTENT_LIBRARY_PERMISSIONS = {
   MANAGE_LIBRARY_TAGS: 'content_libraries.manage_library_tags',
   VIEW_LIBRARY: 'content_libraries.view_library',
 
+  CREATE_LIBRARY_CONTENT: 'content_libraries.create_library_content',
   EDIT_LIBRARY_CONTENT: 'content_libraries.edit_library_content',
+  DELETE_LIBRARY_CONTENT: 'content_libraries.delete_library_content',
   PUBLISH_LIBRARY_CONTENT: 'content_libraries.publish_library_content',
   REUSE_LIBRARY_CONTENT: 'content_libraries.reuse_library_content',
+  IMPORT_LIBRARY_CONTENT: 'content_libraries.import_library_content',
+
+  MANAGE_LIBRARY_TEAM: 'content_libraries.manage_library_team',
+  VIEW_LIBRARY_TEAM: 'content_libraries.view_library_team',
 
   CREATE_LIBRARY_COLLECTION: 'content_libraries.create_library_collection',
   EDIT_LIBRARY_COLLECTION: 'content_libraries.edit_library_collection',
   DELETE_LIBRARY_COLLECTION: 'content_libraries.delete_library_collection',
 
-  MANAGE_LIBRARY_TEAM: 'content_libraries.manage_library_team',
-  VIEW_LIBRARY_TEAM: 'content_libraries.view_library_team',
 };
 
 // Note: this information will eventually come from the backend API
@@ -29,25 +33,104 @@ export const libraryRolesMetadata: RoleMetadata[] = [
 export const libraryResourceTypes: ResourceMetadata[] = [
   { key: 'library', label: 'Library', description: 'Permissions related to the library as a whole.' },
   { key: 'library_content', label: 'Content', description: 'Permissions to create, edit, delete, and publish individual content items within the library.' },
-  { key: 'library_collection', label: 'Collection', description: 'Permissions to create, edit, and delete content collections within the library.' },
   { key: 'library_team', label: 'Team', description: 'Permissions to manage user access and roles within the library.' },
+  { key: 'library_collection', label: 'Collection', description: 'Permissions to create, edit, and delete content collections within the library.' },
 ];
 
 export const libraryPermissions: PermissionMetadata[] = [
-  { key: CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY, resource: 'library', description: 'Allows the user to delete the library and all its contents.' },
-  { key: CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TAGS, resource: 'library', description: 'Add or remove tags from content.' },
   { key: CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY, resource: 'library', description: 'View content, search, filter, and sort within the library.' },
+  { key: CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TAGS, resource: 'library', description: 'Add or remove tags from content.' },
+  { key: CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY, resource: 'library', description: 'Allows the user to delete the library and all its contents.' },
 
+  { key: CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_CONTENT, resource: 'library_content', description: 'Create content within the library.' },
   { key: CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_CONTENT, resource: 'library_content', description: 'Edit content in draft mode' },
+  { key: CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_CONTENT, resource: 'library_content', description: 'Delete content within the library.' },
   { key: CONTENT_LIBRARY_PERMISSIONS.PUBLISH_LIBRARY_CONTENT, resource: 'library_content', description: 'Publish content, making it available for reuse' },
   { key: CONTENT_LIBRARY_PERMISSIONS.REUSE_LIBRARY_CONTENT, resource: 'library_content', description: 'Reuse published content within a course.' },
+  { key: CONTENT_LIBRARY_PERMISSIONS.IMPORT_LIBRARY_CONTENT, resource: 'library_content', description: 'Import content from courses.' },
+
+  { key: CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM, resource: 'library_team', description: 'Add, remove, and assign roles to users within the library.' },
+  { key: CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TEAM, resource: 'library_team', description: 'View the list of users who have access to the library.' },
 
   { key: CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_COLLECTION, resource: 'library_collection', description: 'Create new collections within a library.' },
   { key: CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_COLLECTION, resource: 'library_collection', description: 'Add or remove content from existing collections.' },
   { key: CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_COLLECTION, resource: 'library_collection', description: 'Delete entire collections from the library.' },
+];
 
-  { key: CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TEAM, resource: 'library_team', description: 'View the list of users who have access to the library.' },
-  { key: CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM, resource: 'library_team', description: 'Add, remove, and assign roles to users within the library.' },
+export const rolesLibraryObject = [
+  {
+    role: 'library_admin',
+    permissions: [
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY,
+      CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TAGS,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.PUBLISH_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM,
+      CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TEAM,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.REUSE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.IMPORT_LIBRARY_CONTENT,
+    ],
+    user_count: 1,
+    name: 'Library Admin',
+    description: 'The Library Admin has full control over the library, including managing users, modifying content, and handling publishing workflows. They ensure content is properly maintained and accessible as needed.',
+  },
+  {
+    role: 'library_author',
+    permissions: [
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY,
+      CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TAGS,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.PUBLISH_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.REUSE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.IMPORT_LIBRARY_CONTENT
+    ],
+    user_count: 1,
+    name: 'Library Author',
+    description: 'The Library Author is responsible for creating, editing, and publishing content within a library. They can manage tags and collections but cannot delete libraries or manage users.',
+  },
+  {
+    role: 'library_contributor',
+    permissions: [
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY,
+      CONTENT_LIBRARY_PERMISSIONS.MANAGE_LIBRARY_TAGS,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.REUSE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.EDIT_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_COLLECTION,
+      CONTENT_LIBRARY_PERMISSIONS.CREATE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.DELETE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.IMPORT_LIBRARY_CONTENT
+
+    ],
+    user_count: 1,
+    name: 'Library Contributor',
+    description: 'The Library Contributor can create and edit content within a library but cannot publish it. They support the authoring process while leaving final publishing to Authors or Admins.',
+  },
+  {
+    role: 'library_user',
+    permissions: [
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY,
+      CONTENT_LIBRARY_PERMISSIONS.REUSE_LIBRARY_CONTENT,
+      CONTENT_LIBRARY_PERMISSIONS.VIEW_LIBRARY_TEAM,
+    ],
+    user_count: 1,
+    name: 'Library User',
+    description: 'The Library User can view and reuse content but cannot edit or delete anything.',
+  },
 ];
 
 export const DEFAULT_TOAST_DELAY = 5000;
