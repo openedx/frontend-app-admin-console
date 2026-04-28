eb12ea174e1c78ccf99925c987c9a468955b6ac5
Jesus Balderrama <jesus.balderrama.wgu@gmail.com>
2026-04-28 09:26:29 -0600

    fix: replaced space for + char to render the filter correctly in the UI (#140)
    * fix: replaced space for + char to render the filter correctly in the UI
    
    * chore: comment added to scope param

--- src/authz-module/authz-home/index.tsx
+++ src/authz-module/authz-home/index.tsx
@@ -12,7 +12,11 @@ const AuthzHome = () => {
   const { hash } = useLocation();
   const intl = useIntl();
   const [searchParams] = useSearchParams();
-  const presetScope = searchParams.get('scope') || undefined;
+
+  /* In the authoring repository the scope is encoded but this is to Replace spaces with '+'
+     to match URL encoding format in case the 'scope' parameter is provided with spaces instead of '+'
+  */
+  const presetScope = searchParams.get('scope')?.replace(/\s/g, '+') || undefined;
 
   const pageTitle = intl.formatMessage(messages['authz.manage.page.title']);
 
