4916b126af5fba2a95266a62f85ae4496901cff2
Diana Olarte <diana.olarte@edunext.co>
2026-06-30 16:55:36 +1000

    fix: filter roles displays only the user scope related

--- src/authz-module/components/constants.ts
+++ src/authz-module/components/constants.ts
@@ -8,12 +8,14 @@ export const getRolesFiltersOptions = (intl: IntlShape) => [
     groupIcon: Language,
     displayName: 'Super Admin',
     value: 'super_admin',
+    contextType: 'global',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.global']),
     groupIcon: Language,
     displayName: 'Global Staff',
     value: 'global_staff',
+    contextType: 'global',
   },
 
   {
@@ -21,24 +23,28 @@ export const getRolesFiltersOptions = (intl: IntlShape) => [
     groupIcon: School,
     displayName: 'Course Admin',
     value: 'course_admin',
+    contextType: 'course',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.courses']),
     groupIcon: School,
     displayName: 'Course Staff',
     value: 'course_staff',
+    contextType: 'course',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.courses']),
     groupIcon: School,
     displayName: 'Course Editor',
     value: 'course_editor',
+    contextType: 'course',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.courses']),
     groupIcon: School,
     displayName: 'Course Auditor',
     value: 'course_auditor',
+    contextType: 'course',
   },
 
   {
@@ -46,24 +52,28 @@ export const getRolesFiltersOptions = (intl: IntlShape) => [
     groupIcon: LibraryBooks,
     displayName: 'Library Admin',
     value: 'library_admin',
+    contextType: 'library',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.libraries']),
     groupIcon: LibraryBooks,
     displayName: 'Library Author',
     value: 'library_author',
+    contextType: 'library',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.libraries']),
     groupIcon: LibraryBooks,
     displayName: 'Library Contributor',
     value: 'library_contributor',
+    contextType: 'library',
   },
   {
     groupName: intl.formatMessage(messages['authz.team.members.table.group.libraries']),
     groupIcon: LibraryBooks,
     displayName: 'Library User',
     value: 'library_user',
+    contextType: 'library',
   },
 ];
 
