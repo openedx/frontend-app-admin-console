1afbc4f6c74fadc4081748326c3cefa2c1691932
Diana Olarte <diana.olarte@edunext.co>
2026-07-28 15:28:03 +1000

    refactor: standardize the course & library roles-permissions data (#170)
    * fix(authz): correct library permission labels and relocate permission-matrix utils
    
    * style: display multiline role-metadata to improve reditability
    
    * fix: align libraries table with backend documentation
    
    * fix: align permissions with the backend
    
    * fix: update files description

--- src/authz-module/roles-permissions/messages.ts
+++ src/authz-module/roles-permissions/messages.ts
@@ -1,63 +1,63 @@
 import { defineMessages } from '@edx/frontend-platform/i18n';
 
 const messages = defineMessages({
-  'library.authz.tabs.permissions': {
-    id: 'library.authz.tabs.permissions',
+  'authz.tabs.permissions': {
+    id: 'authz.tabs.permissions',
     defaultMessage: 'Permissions',
     description: 'Libraries AuthZ title for the permissions tab',
   },
-  'library.authz.tabs.permissionsRoles': {
-    id: 'library.authz.tabs.permissionsRoles',
+  'authz.tabs.permissionsRoles': {
+    id: 'authz.tabs.permissionsRoles',
     defaultMessage: 'Roles and Permissions',
     description: 'Libraries AuthZ title for the permissions and roles tab',
   },
-  'library.authz.tabs.permissionsRoles.courses.alert.title': {
-    id: 'library.authz.tabs.permissionsRoles.courses.alert.title',
+  'authz.tabs.permissionsRoles.courses.alert.title': {
+    id: 'authz.tabs.permissionsRoles.courses.alert.title',
     defaultMessage: 'Course Roles',
     description: 'Libraries AuthZ title for the course roles alert',
   },
-  'library.authz.tabs.permissionsRoles.courses.tab': {
-    id: 'library.authz.tabs.permissionsRoles.courses.tab',
+  'authz.tabs.permissionsRoles.courses.tab': {
+    id: 'authz.tabs.permissionsRoles.courses.tab',
     defaultMessage: 'Courses',
     description: 'Libraries AuthZ title for the course roles tab',
   },
-  'library.authz.tabs.permissionsRoles.libraries.tab': {
-    id: 'library.authz.tabs.permissionsRoles.libraries.tab',
+  'authz.tabs.permissionsRoles.libraries.tab': {
+    id: 'authz.tabs.permissionsRoles.libraries.tab',
     defaultMessage: 'Libraries',
     description: 'Libraries AuthZ title for the libraries roles tab',
   },
-  'library.authz.tabs.permissionsRoles.libraries.tab.title': {
-    id: 'library.authz.tabs.permissionsRoles.libraries.tab.title',
+  'authz.tabs.permissionsRoles.libraries.tab.title': {
+    id: 'authz.tabs.permissionsRoles.libraries.tab.title',
     defaultMessage: 'Library Roles',
     description: 'Libraries AuthZ title for the library roles table',
   },
-  'library.authz.tabs.permissionsRoles.courses.tab.title': {
-    id: 'library.authz.tabs.permissionsRoles.courses.tab.title',
+  'authz.tabs.permissionsRoles.courses.tab.title': {
+    id: 'authz.tabs.permissionsRoles.courses.tab.title',
     defaultMessage: 'Course Roles',
     description: 'Libraries AuthZ title for the course roles table',
   },
-  'library.authz.tabs.permissionsRoles.courses.alert.note': {
-    id: 'library.authz.tabs.permissionsRoles.courses.alert.note',
+  'authz.tabs.permissionsRoles.courses.alert.note': {
+    id: 'authz.tabs.permissionsRoles.courses.alert.note',
     defaultMessage: 'Note:',
     description: 'Libraries AuthZ note for the course roles alert',
   },
-  'library.authz.tabs.permissionsRoles.courses.alert.description': {
-    id: 'library.authz.tabs.permissionsRoles.courses.alert.description',
+  'authz.tabs.permissionsRoles.courses.alert.description': {
+    id: 'authz.tabs.permissionsRoles.courses.alert.description',
     defaultMessage: 'This list shows the permissions currently available in Authoring Studio. Some roles may grant additional permissions managed outside this interface.',
     description: 'Libraries AuthZ description for the course roles alert',
   },
-  'library.authz.tabs.permissionsRoles.courses.alert.link': {
-    id: 'library.authz.tabs.permissionsRoles.courses.alert.link',
+  'authz.tabs.permissionsRoles.courses.alert.link': {
+    id: 'authz.tabs.permissionsRoles.courses.alert.link',
     defaultMessage: 'See full documentation',
     description: 'Libraries AuthZ link for the course roles alert',
   },
-  'library.authz.team.remove.user.toast.success.description': {
-    id: 'library.authz.team.remove.user.toast.success.description',
+  'authz.team.remove.user.toast.success.description': {
+    id: 'authz.team.remove.user.toast.success.description',
     defaultMessage: 'The {role} role has been successfully removed.{rolesCount, plural, =0 { The user no longer has access to this library and has been removed from the member list.} other {}}',
     description: 'Libraries team management remove user toast success',
   },
-  'library.authz.team.toast.default.error.message': {
-    id: 'library.authz.team.toast.default.error.message',
+  'authz.team.toast.default.error.message': {
+    id: 'authz.team.toast.default.error.message',
     defaultMessage: '<Bold>Something went wrong on our end.</Bold> <Br></Br>Please try again later.',
     description: 'Libraries default error message',
   },
