d3b901fcb4be3fdcb248040bb215710c90d4d194
Jesus Balderrama <jesus.balderrama.wgu@gmail.com>
2026-08-18 15:23:36 -0600

    fix: complete frontend-base migration react helmet added

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,27 @@
-import { CurrentAppProvider, PageWrap } from '@openedx/frontend-base';
+import { CurrentAppProvider, PageWrap, getSiteConfig, useIntl } from '@openedx/frontend-base';
+import { Helmet } from 'react-helmet';
 
 import { appId } from './constants';
+import messages from './messages';
 import AuthZModule from './authz-module';
 
-const Main = () => (
-  <CurrentAppProvider appId={appId}>
-    <PageWrap>
-      <AuthZModule />
-    </PageWrap>
-  </CurrentAppProvider>
-);
+const Main = () => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <CurrentAppProvider appId={appId}>
+      <Helmet>
+        <title>
+          {formatMessage(messages['admin.console.page.title'], {
+            siteName: getSiteConfig().siteName,
+          })}
+        </title>
+      </Helmet>
+      <PageWrap>
+        <AuthZModule />
+      </PageWrap>
+    </CurrentAppProvider>
+  );
+};
 
 export default Main;
