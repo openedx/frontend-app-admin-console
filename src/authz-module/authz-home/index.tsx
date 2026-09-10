c9e51f2c092bb40121f96bb206577e66f2f7f531
Diana Olarte <diana.olarte@edunext.co>
2026-09-10 14:39:42 +1000

    refactor: use barriel for team-members

--- src/authz-module/authz-home/index.tsx
+++ src/authz-module/authz-home/index.tsx
@@ -1,7 +1,7 @@
 import { useIntl } from '@openedx/frontend-base';
 import { Tab, Tabs } from '@openedx/paragon';
 import { useLocation, useSearchParams } from 'react-router-dom';
-import TeamMembersTable from '@src/authz-module/team-members/TeamMembersTable';
+import { TeamMembersTable } from '@src/authz-module/team-members';
 import AddRoleButton from '@src/authz-module/components/AddRoleButton';
 import RolesPermissions from '@src/authz-module/roles-permissions/RolesPermissions';
 import AuthZLayout from '@src/authz-module/components/AuthZLayout';
